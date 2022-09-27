c61148a7bc8a3e0d4b2ddea7786cf369590f9803
paulego <egopaul@inbox.ru>
2022-09-27 10:45:58 +0500

    Creates a folder for downloaded images

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+from pathlib import Path
+
 import requests
 
 
 def get_image():
     url_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    response = requests.get(url_image)
+    headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
+    response = requests.get(url_image, headers=headers)
     response.raise_for_status()
     return response.content
 
 
 def save_image():
     filename = 'hubble.jpeg'
-    with open(filename, 'wb') as file:
+    image_directory = Path.cwd().joinpath('image')
+    Path(image_directory).mkdir(parents=True, exist_ok=True)
+    with open(f'{image_directory}/{filename}', 'wb') as file:
         file.write(get_image())
 
 
