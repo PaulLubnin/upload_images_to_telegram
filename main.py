6abe7da302ea9d35374402cafbd8e9c345ca6c1b
paulego <egopaul@inbox.ru>
2022-09-28 13:42:40 +0500

    Created a function for getting nasa links, corrected the saving of images

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import random
+from datetime import datetime
 from os.path import splitext
 from pathlib import Path
 from urllib.parse import urlsplit, unquote
 
 import requests
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 def get_file_extension(url: str):
@@ -12,41 +17,83 @@ def get_file_extension(url: str):
     return splitext(urlsplit(unquote(url).replace(' ', '_'))[2])[1]
 
 
-def get_all_image_links(link_source: str):
-    """Функция вытаскивает ссылки на картинки. Возвращает список словарей с 'id_launch' и 'links_image'."""
+def get_links_nasa_images() -> list:
+    """Функция получает ссылки на фотографии дня c сайта NASA.
+    Возвращает список словарей с 'image_url' и 'date'."""
+
+    all_apod = []
+    api_nasa = 'https://api.nasa.gov/planetary/apod'
+    params = {
+        'api_key': env('API_KEY'),
+        'count': 30
+    }
+    response = requests.get(api_nasa, params=params)
+    response.raise_for_status()
+
+    for apod in response.json():
+        # print(apod)
+        one_apod = {
+            'date': apod['date'],
+            'image_url': apod['url']
+        }
+        all_apod.append(one_apod)
+
+    return all_apod
+
+
+def get_links_spacex_launch_images() -> list:
+    """Функция вытаскивает ссылки на картинки c сайта SpaceX.
+     Возвращает список словарей с 'id_launch', 'image_url' и 'date'."""
 
     all_launch_photos = []
-    response = requests.get(link_source)
+    api_spacex = 'https://api.spacexdata.com/v5/launches/'
+    response = requests.get(api_spacex)
     response.raise_for_status()
 
     for launch in response.json():
         if launch['links']['flickr']['original']:
             one_launch = {
                 'id_launch': launch['id'],
-                'links_image': launch['links']['flickr']['original']
+                'date': datetime.fromisoformat(launch['date_local']).strftime('%Y-%m-%d'),
+                'image_url': launch['links']['flickr']['original']
             }
             all_launch_photos.append(one_launch)
 
     return all_launch_photos
 
 
-def fetch_spacex_random_launch(array: list):
-    """Функция выбирает случайным образом один запуск, затем создает и сохраняет папку с картинками запуска. """
+def images_directory(path: str):
+    """Функция создает директорию, куда будут сохраняться картинки и возвращает её путь."""
 
-    one_launch = random.choice(array)
-    image_directory = Path.cwd().joinpath(f"image/{one_launch['id_launch']}")
-    Path(image_directory).mkdir(parents=True, exist_ok=True)
+    directory = Path.cwd().joinpath(f"{path}")
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    return directory
 
-    for link_number, link in enumerate(one_launch['links_image'], 1):
-        response = requests.get(link)
-        response.raise_for_status()
-        file_extension = get_file_extension(link)
-        with open(f'{image_directory}/space_{link_number}{file_extension}', 'wb') as file:
-            file.write(response.content)
 
-    print(f'Фотографии сохранены в папку {image_directory}')
+def save_image(array: list):
+    """Функция сохраняет картинки."""
+
+    if array[0].get('id_launch'):
+        one_launch = random.choice(array)
+        for link_number, link in enumerate(one_launch['image_url'], 1):
+            response = requests.get(link)
+            response.raise_for_status()
+            file_extension = get_file_extension(link)
+            path = f'images/spacex/{one_launch["date"]}'
+            with open(f"{images_directory(path)}/space_{link_number}{file_extension}", 'wb') as file:
+                file.write(response.content)
+        print('Фотографии запуска ракет сохранены в папку "images/spacex"')
+    else:
+        for elem_number, elem in enumerate(array):
+            response = requests.get(elem['image_url'])
+            response.raise_for_status()
+            file_extension = get_file_extension(elem['image_url'])
+            path = 'images/nasa'
+            with open(f'{images_directory(path)}/apod_{elem_number}_{elem["date"]}{file_extension}', 'wb') as file:
+                file.write(response.content)
+        print('Фотографии дня сохранены в папку "images/nasa"')
 
 
 if __name__ == '__main__':
-    api_spacex = 'https://api.spacexdata.com/v5/launches/'
-    fetch_spacex_random_launch(get_all_image_links(api_spacex))
+    save_image(get_links_spacex_launch_images())
+    save_image(get_links_nasa_images())
