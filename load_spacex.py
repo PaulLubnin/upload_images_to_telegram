caf2a93aa8b5aeb2f6c4483302784cdd5458798a
paulego <egopaul@inbox.ru>
2022-10-04 15:05:12 +0500

    Fixed variable name

--- load_spacex.py
+++ load_spacex.py
@@ -5,24 +5,24 @@ import requests
 import photo_upload
 
 
-def get_random_id_launch() -> str:
+def get_random_launch_id() -> str:
     """Функция для получения рандомного идентификатора запуска."""
 
     all_spacex = 'https://api.spacexdata.com/v5/launches/'
     response = requests.get(all_spacex)
     response.raise_for_status()
     launches = response.json()
-    id_launches = [launch['id'] for launch in launches]
+    launch_ids = [launch['id'] for launch in launches]
 
-    return random.choice(id_launches)
+    return random.choice(launch_ids)
 
 
-def get_links_spacex_launch_images(id_launch: str = None) -> list:
+def get_links_spacex_launch_images(launch_id: str = None) -> list:
     """Функция вытаскивает ссылки на картинки c сайта SpaceX,
      либо по заданному 'id', либо случайный вариант. Возвращает список из урлов."""
 
-    api_spacex = f'https://api.spacexdata.com/v5/launches/{id_launch}' \
-        if id_launch else f'https://api.spacexdata.com/v5/launches/{get_random_id_launch()}'
+    api_spacex = f'https://api.spacexdata.com/v5/launches/{launch_id}' \
+        if launch_id else f'https://api.spacexdata.com/v5/launches/{get_random_launch_id()}'
     response = requests.get(api_spacex)
     response.raise_for_status()
     launch = response.json()
