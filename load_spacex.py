ce0eb3a2d2c4da1e1cb53a2315e651e25151f109
paulego <egopaul@inbox.ru>
2022-10-04 14:57:03 +0500

    Got rid of unnecessary requests, dried up the code

--- load_spacex.py
+++ load_spacex.py
@@ -1,43 +1,35 @@
 import random
-from datetime import datetime
 
 import requests
 
 import photo_upload
 
 
-def get_links_spacex_launch_images(id_launch: str = None) -> dict:
-    """Функция вытаскивает ссылки на картинки c сайта SpaceX,
-     либо по заданному 'id', либо случайный вариант.
-     Возвращает список словарей с 'id_launch', 'image_url' и 'date'."""
+def get_random_id_launch() -> str:
+    """Функция для получения рандомного идентификатора запуска."""
 
-    selected_launch = []
+    all_spacex = 'https://api.spacexdata.com/v5/launches/'
+    response = requests.get(all_spacex)
+    response.raise_for_status()
+    launches = response.json()
+    id_launches = [launch['id'] for launch in launches]
+
+    return random.choice(id_launches)
+
+
+def get_links_spacex_launch_images(id_launch: str = None) -> list:
+    """Функция вытаскивает ссылки на картинки c сайта SpaceX,
+     либо по заданному 'id', либо случайный вариант. Возвращает список из урлов."""
+
     api_spacex = f'https://api.spacexdata.com/v5/launches/{id_launch}' \
-        if id_launch else 'https://api.spacexdata.com/v5/launches/'
+        if id_launch else f'https://api.spacexdata.com/v5/launches/{get_random_id_launch()}'
     response = requests.get(api_spacex)
     response.raise_for_status()
+    launch = response.json()
 
-    if not id_launch:
-        for launch in response.json():
-            if launch['links']['flickr']['original']:
-                one_launch = {
-                    'id_launch': launch['id'],
-                    'date': datetime.fromisoformat(launch['date_local']).strftime('%Y-%m-%d'),
-                    'image_url': launch['links']['flickr']['original']
-                }
-                selected_launch.append(one_launch)
-        selected_launch = random.choice(selected_launch)
-    else:
-        if not response.json()['links']['flickr']['original']:
-            print('Selected launch has no photos')
-        else:
-            one_launch = {
-                'id_launch': response.json()['id'],
-                'date': datetime.fromisoformat(response.json()['date_local']).strftime('%Y-%m-%d'),
-                'image_url': response.json()['links']['flickr']['original']
-            }
-            selected_launch = one_launch
-    return selected_launch
+    if not launch['links']['flickr']['original']:
+        print('Selected launch has no photos')
+    return launch['links']['flickr']['original']
 
 
 if __name__ == '__main__':
