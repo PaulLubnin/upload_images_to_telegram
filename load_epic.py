008541c22cd275f5e9f7cc92078a370d64e26528
paulego <egopaul@inbox.ru>
2022-10-17 17:01:03 +0500

    Removed an unordered feature.

--- load_epic.py
+++ load_epic.py
@@ -1,12 +1,10 @@
-import argparse
-
 import requests
 from environs import Env
 
 from boot_scripts import create_data, save_image
 
 
-def get_links_nasa_epic(nasa_api_key, quantity_epic: int = None):
+def get_links_nasa_epic(nasa_api_key):
     """Функция сохраняет фотографии EPIC c сайта NASA."""
 
     api_epic_url = 'https://epic.gsfc.nasa.gov/api/natural'
@@ -25,21 +23,9 @@ def main():
     env = Env()
     env.read_env()
 
-    parser = argparse.ArgumentParser(
-        prog='load_epic.py',
-        description='Loading images from the selected source.'
-    )
-    parser.add_argument('-qe', '--quantity_epic', type=int, default=1,
-                        help='Quantity of EPIC photos uploaded.')
-    args = parser.parse_args()
-
-    if args.quantity_epic:
-        print(f'Uploading EPIC photos')
-        get_links_nasa_epic(env('NASA_API_KEY'))
-        print('NASA photos saved in "images/nasa/epic/" folder')
-
-    else:
-        print('Unknown command.')
+    print(f'Uploading EPIC photos')
+    get_links_nasa_epic(env('NASA_API_KEY'))
+    print('NASA photos saved in "images/nasa/epic/" folder')
 
 
 if __name__ == '__main__':
